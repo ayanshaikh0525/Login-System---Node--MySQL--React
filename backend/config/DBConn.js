6d71c6c3e82425c8f6577821e35a83f93df82eab
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-03-07 22:26:25 +0530

    Update DBConn.js

--- backend/config/DBConn.js
+++ backend/config/DBConn.js
@@ -9,7 +9,6 @@ const DBConn = async () => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,   // IMPORTANT
       waitForConnections: process.env.DB_WAITFORCONNECTIONS,
       connectionLimit: process.env.DB_CONNECTIONLIMIT,  
       queueLimit: process.env.DB_QUEUELIMIT
